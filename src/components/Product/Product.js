9dbafada0b62ce832e868f72507f0229afa0dd98
MDABDULLAH16 <mdabdullah161036@gmail.com>
2022-11-01 01:09:13 +0600

    add event handler with perams

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import './Product.css'
 
-const Product = ({product}) => {
-    const {img, name,price,ratings,seller}=product;
-   
+
+const Product = ( props ) => {
+    const {handleAddToCart,product}=props
+    const { img, name, price, ratings, seller } = props.product;
+    
+
+
     return (
         <div className='cart-container'>
             <img src={img} alt="" />
             <div className="product-info">
-            <p className='product-name'>{name.slice(0,20)}</p>
+                <p className='product-name'>{name.slice(0, 20)}</p>
                 <p>Price:{price}</p>
                 <p>Seller: {seller}</p>
-                <p>Rating:{ratings} stars</p>
+                <p>Rating:{ratings} stars </p>
             </div>
             <div>
-                <button className='btn-cart'><p>Add To Cart</p></button>
+                <button onClick={()=>handleAddToCart(product)} className='btn-cart'><p>Add To Cart  </p></button>
             </div>
 
         </div>
     );
 };
-
+// {/* <FontAwesomeIco icon="fa-solid fa-cart-shopping" /> */}
 export default Product;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -9,6 +9,9 @@ const Shop = () => {
         .then(res => res.json())
         .then(data =>setProducts(data))
     },[])
+    const handleAddToCart =(product)=>{
+        console.log('clicked add to',product);
+    }
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -18,6 +21,7 @@ const Shop = () => {
                         <Product
                         key={product.id}
                         product={product}
+                        handleAddToCart={handleAddToCart}
                         ></Product>
                     ))
                 }
